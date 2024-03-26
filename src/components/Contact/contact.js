dd0a3738172aa575c742ac2e5d7f54b63867c411
Vishal <Vishalkodam1234@gmail.com>
2024-03-26 13:31:39 -0400

    Updated Social Media Icons using fontawesome-react

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import './contact.css';
-import LinkedIn from '../../assets/linkedin.jpg'
-import Github from '../../assets/github.png'
-import Instagram from '../../assets/instagram.jpg'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
   return (
     <section id='contactPage'>
-        <div id='contact'>
-            <h1 className='contactPageTitle'>Contact Me</h1>
-            <span className='contactDesc'>Please fill out the form for possible collaborations/any work opportunities</span>
-            <form className='contactForm'></form>
-                <input type='text' className='name' placeholder='Your Name'/>
-                <input type='email' className='email'placeholder='Your E-mail'/>
-                <textarea className='msg' name='message'rows="6" placeholder='Your Message'></textarea>
-                <button type= 'submit' value='Send' className='submitBtn'>Submit</button>
-                <div className='links'>
-                    <img src={LinkedIn} alt='LinkedIn' className='link' />
-                    <img src={Github} alt='Github' className='link' />
-                    <img src={Instagram} alt='Instagram' className='link' />
-                </div>
+      <div id='contact'>
+        <h1 className='contactPageTitle'>Contact Me</h1>
+        <span className='contactDesc'>Please fill out the form for possible collaborations/any work opportunities</span>
+        <form className='contactForm'>
+          <input type='text' className='name' placeholder='Your Name'/>
+          <input type='email' className='email' placeholder='Your E-mail'/>
+          <textarea className='msg' name='message' rows="6" placeholder='Your Message'></textarea>
+          <button type='submit' value='Send' className='submitBtn'>Submit</button>
+        </form>
+        <div className='links'>
+          <a href="https://www.linkedin.com/in/vishal-kodam/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} alt='LinkedIn' className='link' />
+          </a>
+          <a href="https://github.com/vishalkodam" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} alt='Github' className='link' />
+          </a>
+          <a href="https://www.instagram.com/__vishal017" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} alt='Instagram' className='link' />
+          </a>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './intro.css';
-import btnImg from '../../assets/hireme.jpg'
-import {Link} from 'react-scroll';
+// import btnImg from '../../assets/hireme.jpg'
+// import {Link} from 'react-scroll';
 
 const Intro = () => {
   return (
@@ -9,7 +9,7 @@ const Intro = () => {
         <div className="introContent">
             <span className="introName">Vishal Kodam</span>
             <p className= "introPara">TECH --Innovation | Research | Social</p>
-            <Link><button className='btn'><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
+            {/* <Link><button className='btn'><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link> */}
         </div>
     </section>
   )
