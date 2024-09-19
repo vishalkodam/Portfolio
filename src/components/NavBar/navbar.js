b12e2ab5add0758006d448287f79a4bf47e4e3e1
Vishal <Vishalkodam1234@gmail.com>
2024-09-19 15:15:55 -0400

    .

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -3,30 +3,31 @@ import './navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import lightModeIcon from '../../assets/Sun.svg'; // Import light mode icon
-import darkModeIcon from '../../assets/Moon.svg'; // Import dark mode icon
 import { Link } from 'react-scroll';
 import menu from '../../assets/menu.png';
 import resumeFile from '../../assets/Resume.pdf';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
 
-  // Function to toggle between light and dark mode
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    // Additional logic to toggle dark mode styles across the application
-    // For example, you might want to update CSS variables or apply different classes
+  // Function to show the motivational message
+  const showMotivationalMessage = () => {
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 5000); // Hide the message after 5 seconds
   };
 
   return (
     <nav className="navbar">
-      <button className="toggleBtn" onClick={toggleDarkMode}>
+      <button className="toggleBtn" onClick={showMotivationalMessage}>
         <img
-          src={isDarkMode ? darkModeIcon : lightModeIcon}
-          alt={isDarkMode ? 'Dark Mode' : 'Light Mode'}
+          src={lightModeIcon}
+          alt="Light Mode"
           className="toggleIcon"
         />
+        {showMessage && (
+          <div className="message">On a Job HUNT! 🏹</div>
+        )}
       </button>
 
       <div className="desktopMenu">
@@ -35,13 +36,11 @@ const Navbar = () => {
         <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Contact Me</Link>
       </div>
 
-      {/* Resume button */}
       <button className="desktopMenuBtn" onClick={() => { window.open(resumeFile, "_blank") }}>
         <FontAwesomeIcon icon={faDownload} className="downloadIcon" />
         <span>Resume</span>
       </button>
 
-      {/* Menubar */}
       <img src={menu} alt='Menu' className='mobMenu' onClick={() => setShowMenu(!showMenu)} />
       <div className="navMenu" style={{ display: showMenu ? 'flex' : 'none' }}>
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Home</Link>
